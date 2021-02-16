50ab787315f0b83e71e79b3d1736ebe4cefe4393
Jihad Noureddine <jihad.nd@gmail.com>
2021-02-16 13:42:55 +0200

    migration updated - description dropped from the visual table and descrete description table is created

---
+++ database/migrations/2021_02_16_113919_create_visuals_description_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisualsDescriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visuals_description', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('visual_id');
+            $table->foreign('visual_id')->references('id')->on('visuals');
+            
+            $table->mediumText("description_in_arabic")->nullable(false);
+            $table->mediumText("description_in_english")->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visuals_description');
+    }
+}
